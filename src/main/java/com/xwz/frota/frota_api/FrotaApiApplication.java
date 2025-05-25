3f353e36595c2a2b11dc98b2f93640e6c1ca7b42
pedro-lucasMS <pedro.lmarinho2002@gmail.com>
2025-05-25 14:12:41 -0300

    git commit -m "chore: estrutura inicial do projeto com Spring Boot e configuração do PostgreSQL"

---
+++ src/main/java/com/xwz/frota/frota_api/FrotaApiApplication.java
@@ -0,0 +1,13 @@
+package com.xwz.frota.frota_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrotaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrotaApiApplication.class, args);
+	}
+
+}
